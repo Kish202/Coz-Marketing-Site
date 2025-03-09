3524d971bae3e4fdea3a1620a7050b084b4f221a
kish202 <tania2020076@gmail.com>
2025-03-09 22:42:52 +0530

    Components

--- frontend/src/components/Banner/Banner.jsx
+++ frontend/src/components/Banner/Banner.jsx
@@ -5,7 +5,7 @@ import Bannerimg from "../../assets/blog2.jpg";
 function Banner({reverse, img}) {
   return (
     <section>
-     <main   className="bg-slate-100 dark:bg-slate-900 dark:text-white">
+     <main   className="bg-slate-100 dark:bg-slate-900 dark:text-white mt-10 py-6">
      <section className="container flex flex-col items-center justify-center py-10 md:h-[500px]">
 
 
@@ -14,18 +14,44 @@ function Banner({reverse, img}) {
  <div 
  className={`flex flex-col items-start gap-4 text-left md:p-8 ${reverse ? "md:order-last": ""}`}>
  <h1 className="text-2xl md:text-4xl">
-     We Builds
+     We Builds apps that get Trending on techworld.
     
       </h1> 
 
 
-      <p>
-        
+      <p classname="text-sm text-slate-600 dark:text-slate-400">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab commodi fugiat nemo 
+        possimus voluptates
+         illo facere iure atque corrupti enim?
         </p>  
+    
+    
+
+    <div>
+
+        <ul className="flex list-inside flex-start  list-disc flex-col gap-2 md:gap-4">
+ 
+             <li className="font-medium ">
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam!
+            </li>
+<li className="font-medium">Lorem ipsum, dolor sit amet consectetur adipisicing T.</li>
+      
+            <li className="font-medium" >
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam!
+            </li>
+        </ul>
+    </div>
+      <div className="space-x-4">
+
+        <button className="rounded-md border-2 border-primary bg-primary px-4 py-2 text-sm text-white duration-300 hover:bg-primary/90">
+GET Started
+        </button>
+    </div>
     </div>       
         
-   <div>
-    
+   <div className={reverse?"order-1":""}>
+    <img 
+    src={img} alt="no image"      className="mx-auto w-full hover:drop-shadow-md h-90%"/>
     </div>     
    
         </div>  
